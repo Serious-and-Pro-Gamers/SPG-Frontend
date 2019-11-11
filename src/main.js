668c7f01e60265937b7637d31be64a4847b15b00
zanderlx <lsaplan97@gmail.com>
2019-11-10 19:56:23 -0800

    Initialized vuetify, router, and axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import router from './plugins/router';
+import axios from './plugins/axios';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  axios,
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
